8a4465bc85c9c692f956781da53dd85bcef9d31c
Keamogetswe Moeketsi <167786154+chantyymore@users.noreply.github.com>
2025-02-24 20:11:08 +0200

    updated the javascrpt, added the weather api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
+function refreshTemperature(response) {
+  //check if it works using console
+  //console.log(response.data.temperature.current);
+  let temperatureElements = document.querySelector("#temperature");
+  temperatureElements.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  //make api call and update the interface
+  let apiKey = "6b534oeab5dabbea88bbtf6452c02346";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query=" +
+    city +
+    "&key=" +
+    apiKey;
+  axios.get(apiURL).then(refreshTemperature);
+}
+
 function handleSearchForm(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
   let cityElement = document.querySelector("#city");
+  //the value written in serchInput replaces cityElement
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
+//select the whole form
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchForm);
+
+searchCity("Paris");
